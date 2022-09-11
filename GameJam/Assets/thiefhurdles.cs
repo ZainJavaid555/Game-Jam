456a5ca4d258a2b7c55679d749e6e61a4758bf99
ZainJavaid555 <zain.javaid545@gmail.com>
2022-09-11 17:01:21 +0500

    Improved Thief fluctuation

--- GameJam/Assets/thiefhurdles.cs
+++ GameJam/Assets/thiefhurdles.cs
@@ -7,6 +7,9 @@ public class thiefhurdles : MonoBehaviour
     public BoxCollider hitCollider;
     public BoxCollider safeCollider;
 
+    public bool thiefHit = false;
+    public float countDown;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,20 +18,39 @@ public class thiefhurdles : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         
+    }*/
+
+    private void Update()
+    {
+        if (thiefHit == true)
+        {
+            hitCollider.isTrigger = true;
+            safeCollider.isTrigger = true;
+        }
+
+        if (thiefHit == false)
+        {
+            /*hitCollider.isTrigger = false;
+            safeCollider.isTrigger = false;*/
+
+            StartCoroutine(StartCounter());
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Thief")
+        if (collision.gameObject.tag == "Thief")
         {
             Debug.Log("Thief is colliding");
 
-            hitCollider.isTrigger = true;
-            safeCollider.isTrigger = true;
-            
+            thiefHit = true;
+
+            //hitCollider.isTrigger = true;
+            //safeCollider.isTrigger = true;
+
         }
     }
 
@@ -38,8 +60,28 @@ public class thiefhurdles : MonoBehaviour
         {
             Debug.Log("Thief is out of collider");
 
-            hitCollider.isTrigger = false;
-            safeCollider.isTrigger = false;
+            thiefHit = false;
+
+            //hitCollider.isTrigger = false;
+            //safeCollider.isTrigger = false;
         }
+    }
+
+    private IEnumerator StartCounter()
+    {
+        countDown = 0.8f;
+        for (int i = 0; i < 10000; i++)
+        {
+            while (countDown >= 0)
+            {
+                
+                countDown -= Time.smoothDeltaTime;
+                
+                yield return null;
+            }
+
+        }
+        hitCollider.isTrigger = false;
+        safeCollider.isTrigger = false;
     }
 }
