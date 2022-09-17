bc2aab60a597509b7b3f5d2c6e43b0e305dce7bb
omar-ahsan <omarkhattak0@gmail.com>
2022-09-17 14:31:03 +0500

    Added script to reduce speed of thief

--- GameJam/Assets/Scripts/FeatherScripts/Pickup.cs
+++ GameJam/Assets/Scripts/FeatherScripts/Pickup.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+
+    ThiefMovement tm;
     private void Start()
     {
         
@@ -13,6 +15,11 @@ public class Pickup : MonoBehaviour
     {
         
     }
+
+    private void Awake()
+    {
+        tm = GetComponent<ThiefMovement>();
+    }
     private void OnTriggerEnter(Collider Col)
     {
         
@@ -20,6 +27,7 @@ public class Pickup : MonoBehaviour
         {
             GetComponent<Points>().points++; // Add 1 Point
             //Decrease speed of thief by 0.03 for each feather.
+            tm.thiefSpeed = tm.thiefSpeed - 0.0120f; // 50 x 0.02 = 1ms , 50 x 0.0160 = 0.8ms , 50 x 0.012 = 0.6ms
             Col.gameObject.SetActive(false);
         }
     }
