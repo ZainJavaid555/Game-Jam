f099d973af51da552a4a4238f383b34a2ef126f8
omar-ahsan <omarkhattak0@gmail.com>
2022-09-18 22:07:44 +0500

    Added real feather values

--- GameJam/Assets/Scripts/FeatherScripts/Pickup.cs
+++ GameJam/Assets/Scripts/FeatherScripts/Pickup.cs
@@ -12,57 +12,57 @@ public class Pickup : MonoBehaviour
         
     }
 
-    //private void FixedUpdate()
-    //{
-    //    if(GetComponent<Points>().points == 40 && tm.thiefSpeed >= 0)
-    //    {
-    //        tm.thiefSpeed = 5.5f;
-    //        Debug.Log("Thief speed is 5.5");
-    //    }
-
-    //    else if (GetComponent<Points>().points == 80 && tm.thiefSpeed >= 0)
-    //    {
-    //        tm.thiefSpeed = 5.25f;
-    //        Debug.Log("Thief speed is 5.25");
-    //    }
-        
-    //    else if (GetComponent<Points>().points == 110 && tm.thiefSpeed >= 0)
-    //    {
-    //        tm.thiefSpeed = 5.0f;
-    //        Debug.Log("Thief speed is 5.0");
-    //    }
-
-    //    else if (GetComponent<Points>().points == 145 && tm.thiefSpeed >= 0)
-    //    {
-    //        tm.thiefSpeed = 4.75f;
-    //        Debug.Log("Thief speed is 4.5");
-    //    }
-
-    //    else if (GetComponent<Points>().points == 160 && tm.thiefSpeed >= 0)
-    //    {
-    //        tm.thiefSpeed = 3.50f;
-    //        Debug.Log("Thief speed is 3.50");
-    //    }
-
-    //}
-
-    // testing end game scenerio
     private void FixedUpdate()
     {
-        if (GetComponent<Points>().points == 20 && tm.thiefSpeed >= 0)
+        if (GetComponent<Points>().points == 40 && tm.thiefSpeed >= 0)
         {
-            tm.thiefSpeed = 4.5f;
+            tm.thiefSpeed = 5.5f;
+            Debug.Log("Thief speed is 5.5");
+        }
+
+        else if (GetComponent<Points>().points == 80 && tm.thiefSpeed >= 0)
+        {
+            tm.thiefSpeed = 5.25f;
+            Debug.Log("Thief speed is 5.25");
+        }
+
+        else if (GetComponent<Points>().points == 110 && tm.thiefSpeed >= 0)
+        {
+            tm.thiefSpeed = 5.0f;
             Debug.Log("Thief speed is 5.0");
         }
 
-        else if (GetComponent<Points>().points == 70 && tm.thiefSpeed >= 0)
+        else if (GetComponent<Points>().points == 145 && tm.thiefSpeed >= 0)
         {
-            tm.thiefSpeed = 4.5f;
+            tm.thiefSpeed = 4.75f;
             Debug.Log("Thief speed is 4.5");
         }
 
+        else if (GetComponent<Points>().points == 160 && tm.thiefSpeed >= 0)
+        {
+            tm.thiefSpeed = 3.50f;
+            Debug.Log("Thief speed is 3.50");
+        }
 
     }
+
+    //// testing end game scenerio
+    //private void FixedUpdate()
+    //{
+    //    if (GetComponent<Points>().points == 20 && tm.thiefSpeed >= 0)
+    //    {
+    //        tm.thiefSpeed = 4.5f;
+    //        Debug.Log("Thief speed is 5.0");
+    //    }
+
+    //    else if (GetComponent<Points>().points == 70 && tm.thiefSpeed >= 0)
+    //    {
+    //        tm.thiefSpeed = 4.5f;
+    //        Debug.Log("Thief speed is 4.5");
+    //    }
+
+
+    //}
 
     private void Awake()
     {
